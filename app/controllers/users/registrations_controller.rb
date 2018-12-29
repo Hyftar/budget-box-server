3f7ab1ef66568a1cc0826d3edd423b06f75a01ef
Simon Landry <landry_simon@pm.me>
2018-12-29 01:06:58 -0500

    Add not_authenticated and authenticated layout, add title helper

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout 'not_authenticated'
+
   private
 
   def sign_up_params
--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Users::UnlocksController < Devise::UnlocksController
+  layout 'not_authenticated', only: [:new]
 end
